d1cecf264b8d7a0efae5c7cd8eab247b8f8c3a0a
Jay Murphy <jmrphyii@memphis.edu>
2026-03-30 18:42:24 -0500

    feat: customer fix

---
+++ resources/views/emails/customer-welcome-text.blade.php
@@ -0,0 +1,11 @@
+Welcome to {{ $tenantName }}!
+
+Hi {{ $userName }},
+
+An account has been created for you on the {{ $tenantName }} customer portal.
+
+Click the link below to sign in (expires in 72 hours):
+
+{{ $loginUrl }}
+
+After signing in, you can request a new sign-in link from the portal login page at any time.
