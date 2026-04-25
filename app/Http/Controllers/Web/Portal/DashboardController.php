06beb0ba4330509bff38ae84b9164122f472c327
Jay Murphy <jmrphyii@memphis.edu>
2026-04-24 22:48:25 -0500

    fix typ0

--- app/Http/Controllers/Web/Portal/DashboardController.php
+++ app/Http/Controllers/Web/Portal/DashboardController.php
@@ -25,7 +25,7 @@ class DashboardController extends Controller
             ->map(fn ($dog) => [
                 'id'             => $dog->id,
                 'name'           => $dog->name,
-                'breed'          => $dog->breed? $dog->breed->breed : null,
+                'breed'          => $dog->breed? $dog->breed->name : null,
                 'credit_balance' => $dog->credit_balance,
                 'credits_expire_at' => $dog->credits_expire_at?->toIso8601String(),
                 'credit_status'  => match (true) {
